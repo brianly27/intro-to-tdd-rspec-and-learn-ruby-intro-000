d109879dea13fcd1e770fb13be0d8c699d846ca1
Brian Ly <brian.kyang.ly@gmail.com>
2019-06-20 17:15:33 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
-def greeting(name, language = "Ruby")
+def greeting(name="Ruby Programmer", language)
   puts "Hello #{name}, We heard you are a great #{language} programmer"
 end
 
-greeting("Brian")
+greeting("C++")
