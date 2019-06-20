993c4079c943c63b3a45044278c896ef1408d816
Brian Ly <brian.kyang.ly@gmail.com>
2019-06-20 17:06:36 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,7 +1,5 @@
-require_relative './current_age_for_birth_year.rb'
+def greeting(name = "Ruby Programmer", language = "Ruby")
+  puts "Hello #{name}, We heard you are a great #{language} programmer"
+end
 
-puts "What year were you born?"
-birth_year = gets.to_i
-users_age = current_age_for_birth_year(birth_year)
-
-puts "You are " + users_age.to_s + " years old."
+greeting("Brian", "Python")
